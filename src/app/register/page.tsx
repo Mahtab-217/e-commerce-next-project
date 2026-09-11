cff5b26d83d6909671e3ef41bf9cbb9c725028a3
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-09-11 21:18:18 +0430

    new codes added

---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className=' w-full h-screen flex justify-center items-center'>
+      <h1>Form</h1>
+    </div>
+  )
+}
+
+export default page
